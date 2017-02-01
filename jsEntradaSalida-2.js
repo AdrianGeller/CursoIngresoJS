700f6117382fe41a069525e11ead7a50eda3012e
AdrianGeller <elgorigeller@hotmail.com>
2017-02-01 12:03:00 -0300

    Add files via upload

---
+++ jsEntradaSalida-2.js
@@ -0,0 +1,15 @@
+/*Debemos lograr tomar un nombre con 'prompt' 
+y luego mostrarlo por 'alert' al presionar el botón  'MOSTRAR'*/
+function Mostar()
+{
+	//esto es un comentario de una línea
+	/*esto es un comentario
+	entre varias líneas
+	*/	
+var nombre; //defino variable
+nombre = "Adrián"; //cargo variable
+nombre = prompt ("ingrese su nombre", "Natalia Natalia"); //sobreescritura de variables, el prompt se usa con una variable declarada
+alert ("Nombre: " + nombre); //concatenar variables, el alert se usa sin declarar antes una variable
+var person = prompt("Ingresá tu nombre", "Natalia Natalia");
+}
+
