a6f4416ea5ee8c39b1649c6d98a4629cd418c74e
AdrianGeller <elgorigeller@hotmail.com>
2017-02-22 11:39:38 -0300

    Add files via upload

---
+++ jsIteracionesFor (1).js
@@ -0,0 +1,47 @@
+function Mostrar()
+{
+
+var contador=0;
+
+//for( contador<10;)dos formas de hacer lo mismo
+/*for(; ;)
+{
+	//contador++;
+	//console.log(contador);
+	if(contador==10)
+	{
+		break;
+	}
+	contador++;
+
+
+}
+*/
+for(; ;)
+{
+	if(contador==10)
+	{
+		break;
+	}
+	contador++;
+	console.log(contador);
+}
+
+/*
+for(;var resp="no";)
+{
+	if(contador==10)
+	{
+		resp="no";
+	}
+	contador++;
+	console.log(contador);
+}
+
+
+
+*/
+
+
+
+}
